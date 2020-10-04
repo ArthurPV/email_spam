a2963f76be3b12862695247241766c34730e2551
Arthur <arthur@arch.localdomain>
2020-10-04 17:05:50 -0400

    clean code

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ let simpleObject = [];
 let namesArray = [];
 let emailsArray = [];
 let idArray = [];
-console.log(typeof simpleObject);
 
 // initialize getData function
 const getData = path => {
@@ -23,31 +22,22 @@ const getData = path => {
 // call function
 getData("./data/all_emails.json");
 
-// get emails data
 // get simple object
-// get beetween array for emails and names
 simpleObject = Object.values(personsData);
-console.log(simpleObject[0].length);
+
+// get beetween array for emails and names
 
 const objectToArray = () => {
 	for (let i=0; i < simpleObject[0].length; i++) {
 		let personsArray = Object.values(simpleObject[0][i]);
-		console.log(personsArray);
 		personsArray = Object.values(personsArray);
-		console.log(personsArray)
 		namesArray.push(personsArray[1]);
 		emailsArray.push(personsArray[2]);
 		idArray.push(personsArray[0]);	
-		console.log(personsArray);
-		console.log(namesArray);
-		console.log(emailsArray);
-		console.log(idArray);
 	}
 }
 // call function
 objectToArray();
-console.log(emailsArray);
-
 // send all emails
 
 async function sendEmail() {
@@ -76,5 +66,5 @@ async function sendEmail() {
 		}
 	})
 }
+// call function
 sendEmail().catch(console.error);
-
